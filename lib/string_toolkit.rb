64471b40df9bde0e3205900831672ccbfa1f6227
Theodor Weinreich <theodor.weinreich@webit.de>
2024-04-23 10:53:03 +0200

    Initial commit

---
+++ lib/string_toolkit.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "string_toolkit/version"
+
+module StringToolkit
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/string_toolkit/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module StringToolkit
+  VERSION = "0.1.0"
+end
